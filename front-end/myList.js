c16910749e6c51cef3007e86fb5becbd906db770
Veeral Tiwari <100480162+VeeralTiwari@users.noreply.github.com>
2024-10-02 00:03:57 +0530

    Update myList.js

--- front-end/myList.js
+++ front-end/myList.js
@@ -3,7 +3,7 @@ const loader = document.querySelector(".loader");
 
 async function fetchData() {
     try {
-        const response = await fetch('http://127.0.0.1:5000', {
+        const response = await fetch('https://anime-recommendation-deployment.onrender.com', {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json'
