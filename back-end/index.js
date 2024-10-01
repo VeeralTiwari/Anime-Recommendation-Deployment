7bc77c708cb58f1483614440e9b5880373ce8025
Veeral Tiwari <100480162+VeeralTiwari@users.noreply.github.com>
2024-10-01 23:42:59 +0530

    Update index.js

--- back-end/index.js
+++ back-end/index.js
@@ -73,5 +73,7 @@ app.delete('/:id', async (req, res) => {
 });
 
 // Start the server
-const PORT = 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
